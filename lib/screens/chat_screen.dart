edc03c0a8afce4efd5f33c119ff619cec92e29d4
rezatrue <rezatrue@gmail.com>
2021-07-27 08:06:01 +0600

    Cloud Store for firebase added

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -10,6 +11,19 @@ class ChatScreen extends StatelessWidget {
                 padding: EdgeInsets.all(8),
                 child: Text('This works!'),
               )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/3Kfc1KEKzIwec8XAQ9Fb/messages')
+              .snapshots()
+              .listen((data) {
+            data.documents.forEach((element) {
+              print(element['test']);
+            });
+          });
+        },
+      ),
     );
   }
 }
